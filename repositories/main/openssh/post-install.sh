818b1138237ae514b8e9f24cee744d6c94581b29
Pascal0577 <181921662+Pascal0577@users.noreply.github.com>
2025-12-29 06:30:16 -0500

    added openssh

---
+++ repositories/main/openssh/post-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+install -v -g sys -m700 -d /var/lib/sshd &&
+
+groupadd -g 50 sshd        &&
+useradd  -c 'sshd PrivSep' \
+         -d /var/lib/sshd  \
+         -g sshd           \
+         -s /bin/false     \
+         -u 50 sshd
